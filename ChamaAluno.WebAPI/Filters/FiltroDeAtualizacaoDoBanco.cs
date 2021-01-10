d87a10ab568ebdb6a42c97e842a8b4ff5f6f61a3
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2021-01-10 00:47:36 -0300

    incluido try ao atualizar o banco

--- ChamaAluno.WebAPI/Filters/FiltroDeAtualizacaoDoBanco.cs
+++ ChamaAluno.WebAPI/Filters/FiltroDeAtualizacaoDoBanco.cs
@@ -31,8 +31,19 @@ namespace ChamaAluno.WebAPI.Filters
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            var unidadeDeTrabalho = (IUnidadeDeTrabalho)context.HttpContext.RequestServices.GetService(typeof(IUnidadeDeTrabalho));
-            unidadeDeTrabalho.Contexto.Database.Migrate();
+            try
+            {
+                var unidadeDeTrabalho = (IUnidadeDeTrabalho)context.HttpContext.RequestServices.GetService(typeof(IUnidadeDeTrabalho));
+                unidadeDeTrabalho.Contexto.Database.Migrate();
+            }
+            catch(Exception e)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 500,
+                    Content = e.Message + '\n' + e.StackTrace
+                };
+            }
         }
     }
 }
